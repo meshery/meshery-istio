29adcf9e942599c4259ecf41b3c91d483bd856f6
Girish Ranganathan <girish.ranganathan@solarwinds.com>
2019-02-14 00:53:49 -0500

    - moved away from concrete istio types to using dynamic client which gives us the flexibility to apply any config change like gateways, virtualservices, destinationrules, deployments, services, etc. without having to write code against concrete types - upgraded libs to support the dynamic client ops - changed file name to supported_ops to be more appropriate  - removed key from supported operation and rather using a map - removed tightly bound code and templates to the bookinfo app - updated protoc generated file with support for getting custom yaml and delete op

--- istio/client.go
+++ istio/client.go
@@ -15,21 +15,16 @@
 package istio
 
 import (
-	"flag"
+	"k8s.io/client-go/dynamic"
 
-	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
-	kube "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 // IstioClient represents an Istio client in Meshery
 type IstioClient struct {
-	k8s                *kube.Clientset
-	istioConfigApi     *rest.RESTClient
-	istioNetworkingApi *rest.RESTClient
+	// k8s              *kube.Clientset
+	k8sDynamicClient dynamic.Interface
 }
 
 func configClient(kubeconfig []byte, contextName string) (*rest.Config, error) {
@@ -44,12 +39,6 @@ func configClient(kubeconfig []byte, contextName string) (*rest.Config, error) {
 		}
 
 		return clientcmd.NewDefaultClientConfig(*ccfg, &clientcmd.ConfigOverrides{}).ClientConfig()
-	} else {
-		kConfig := flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-		flag.Parse()
-		if *kConfig != "" {
-			return clientcmd.BuildConfigFromFlags("", *kConfig)
-		}
 	}
 	return rest.InClusterConfig()
 }
@@ -63,67 +52,10 @@ func newClient(kubeconfig []byte, contextName string) (*IstioClient, error) {
 	config.QPS = 100
 	config.Burst = 200
 
-	k8s, err := kube.NewForConfig(config)
+	dynamicClient, err := dynamic.NewForConfig(config)
 	if err != nil {
 		return nil, err
 	}
-	client.k8s = k8s
-
-	types := runtime.NewScheme()
-	schemeBuilder := runtime.NewSchemeBuilder(
-		func(scheme *runtime.Scheme) error {
-			for _, kind := range istioKnownTypes {
-				scheme.AddKnownTypes(*kind.groupVersion, kind.object, kind.collection)
-			}
-			meta_v1.AddToGroupVersion(scheme, istioConfigGroupVersion)
-			meta_v1.AddToGroupVersion(scheme, istioNetworkingGroupVersion)
-			return nil
-		})
-
-	err = schemeBuilder.AddToScheme(types)
-	if err != nil {
-		return nil, err
-	}
-
-	istioConfig := rest.Config{
-		Host:    config.Host,
-		APIPath: "/apis",
-		ContentConfig: rest.ContentConfig{
-			GroupVersion:         &istioConfigGroupVersion,
-			NegotiatedSerializer: serializer.DirectCodecFactory{CodecFactory: serializer.NewCodecFactory(types)},
-			ContentType:          runtime.ContentTypeJSON,
-		},
-		BearerToken:     config.BearerToken,
-		TLSClientConfig: config.TLSClientConfig,
-		QPS:             config.QPS,
-		Burst:           config.Burst,
-	}
-
-	istioConfigApi, err := rest.RESTClientFor(&istioConfig)
-	client.istioConfigApi = istioConfigApi
-	if err != nil {
-		return nil, err
-	}
-
-	istioNetworking := rest.Config{
-		Host:    config.Host,
-		APIPath: "/apis",
-		ContentConfig: rest.ContentConfig{
-			GroupVersion:         &istioNetworkingGroupVersion,
-			NegotiatedSerializer: serializer.DirectCodecFactory{CodecFactory: serializer.NewCodecFactory(types)},
-			ContentType:          runtime.ContentTypeJSON,
-		},
-		BearerToken:     config.BearerToken,
-		TLSClientConfig: config.TLSClientConfig,
-		QPS:             config.QPS,
-		Burst:           config.Burst,
-	}
-
-	istioNetworkingApi, err := rest.RESTClientFor(&istioNetworking)
-	client.istioNetworkingApi = istioNetworkingApi
-	if err != nil {
-		return nil, err
-	}
-
+	client.k8sDynamicClient = dynamicClient
 	return &client, nil
 }
