f24ee6a761521890c8d47cbde6b89b0edfd8a985
kumarabd <abishekkumar92@gmail.com>
2020-10-13 23:41:46 +0530

    temp fixes
    Signed-off-by: kumarabd <abishekkumar92@gmail.com>

--- istio/client.go
+++ istio/client.go
@@ -31,7 +31,7 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 
 	"github.com/ghodss/yaml"
-	"github.com/layer5io/gokit/models"
+	// "github.com/layer5io/gokit/models"
 	"github.com/layer5io/gokit/utils"
 )
 
@@ -132,21 +132,75 @@ func monkeyPatchingToSupportInsecureConn(data []byte) []byte {
 	return data1
 }
 
+// Kubeconfig is structure of the kubeconfig file
+type Kubeconfig struct {
+	APIVersion string `yaml:"apiVersion,omitempty" json:"apiVersion,omitempty"`
+	Clusters   []struct {
+		Cluster struct {
+			CertificateAuthorityData string `yaml:"certificate-authority-data,omitempty" json:"certificate-authority-data,omitempty"`
+			Server                   string `yaml:"server,omitempty" json:"server,omitempty"`
+		} `yaml:"cluster,omitempty" json:"cluster,omitempty"`
+		Name string `yaml:"name,omitempty" json:"name,omitempty"`
+	} `yaml:"clusters,omitempty" json:"clusters,omitempty"`
+	Contexts []struct {
+		Context struct {
+			Cluster   string `yaml:"cluster,omitempty" json:"cluster,omitempty"`
+			Namespace string `yaml:"namespace,omitempty" json:"namespace,omitempty"`
+			User      string `yaml:"user,omitempty" json:"user,omitempty"`
+		} `yaml:"context,omitempty" json:"context,omitempty"`
+		Name string `yaml:"name,omitempty" json:"name,omitempty"`
+	} `yaml:"contexts,omitempty" json:"contexts,omitempty"`
+	CurrentContext string `yaml:"current-context,omitempty" json:"current-context,omitempty"`
+	Kind           string `yaml:"kind,omitempty" json:"kind,omitempty"`
+	Preferences    struct {
+	} `yaml:"preferences,omitempty" json:"preferences,omitempty"`
+	Users []struct {
+		Name string `yaml:"name,omitempty" json:"name,omitempty"`
+		User struct {
+			Exec struct {
+				APIVersion string   `yaml:"apiVersion,omitempty" json:"apiVersion,omitempty"`
+				Args       []string `yaml:"args,omitempty" json:"args,omitempty"`
+				Command    string   `yaml:"command,omitempty" json:"command,omitempty"`
+				Env        []struct {
+					Name  string `yaml:"name,omitempty" json:"name,omitempty"`
+					Value string `yaml:"value,omitempty" json:"value,omitempty"`
+				} `yaml:"env,omitempty" json:"env,omitempty"`
+			} `yaml:"exec,omitempty" json:"exec,omitempty"`
+			AuthProvider struct {
+				Config struct {
+					AccessToken string    `yaml:"access-token,omitempty" json:"access-token,omitempty"`
+					CmdArgs     string    `yaml:"cmd-args,omitempty" json:"cmd-args,omitempty"`
+					CmdPath     string    `yaml:"cmd-path,omitempty" json:"cmd-path,omitempty"`
+					Expiry      time.Time `yaml:"expiry,omitempty" json:"expiry,omitempty"`
+					ExpiryKey   string    `yaml:"expiry-key,omitempty" json:"expiry-key,omitempty"`
+					TokenKey    string    `yaml:"token-key,omitempty" json:"token-key,omitempty"`
+				} `yaml:"config,omitempty" json:"config,omitempty"`
+				Name string `yaml:"name,omitempty" json:"name,omitempty"`
+			} `yaml:"auth-provider,omitempty" json:"auth-provider,omitempty"`
+			ClientCertificateData string `yaml:"client-certificate-data,omitempty" json:"client-certificate-data,omitempty"`
+			ClientKeyData         string `yaml:"client-key-data,omitempty" json:"client-key-data,omitempty"`
+			Token                 string `yaml:"token,omitempty" json:"token,omitempty"`
+		} `yaml:"user,omitempty,omitempty" json:"user,omitempty,omitempty"`
+	} `yaml:"users,omitempty" json:"users,omitempty"`
+}
+
 // writeKubeconfig creates kubeconfig in local container
 func writeKubeconfig(kubeconfig []byte, contextName string, path string) error {
 
-	yamlConfig := models.Kubeconfig{}
+	yamlConfig := Kubeconfig{}
 	err := yaml.Unmarshal(kubeconfig, &yamlConfig)
 	if err != nil {
 		return err
 	}
 
 	yamlConfig.CurrentContext = contextName
+	fmt.Printf("%+v\n", yamlConfig)
 
 	d, err := yaml.Marshal(yamlConfig)
 	if err != nil {
 		return err
 	}
+	fmt.Printf(string(d))
 
 	err = ioutil.WriteFile(path, d, 0600)
 	if err != nil {
