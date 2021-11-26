6361b36ef0a19b773a3ad1007865e4e42533e9e7
ashish <ashishjaitiwari15112000@gmail.com>
2021-11-26 18:03:34 +0530

    removed redundant file
    Signed-off-by: ashish <ashishjaitiwari15112000@gmail.com>

--- .github/scripts/mesheryconf.sh
+++
@@ -1,8 +0,0 @@
-	mkdir ~/.meshery
-	config='{"contexts":{"local":{"endpoint":"http://localhost:9081","token":"Default","platform":"kubernetes","adapters":[],"channel":"stable","version":"latest"}},"current-context":"local","tokens":[{"location":"auth.json","name":"Default"}]}'
-
-	echo $config > ~/.meshery/config.yaml
-
-	cat ~/.meshery/config.yaml
-	echo '{ "meshery-provider": "Meshery", "token": null }' | jq -c '.token = "'$provider_token'"' > ~/.meshery/auth.json
-	cat ~/.meshery/auth.json	
