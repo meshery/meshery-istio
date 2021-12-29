df2ff220457646cc0dd4c1319f6cf326d9b5926c
leecalcote <leecalcote@gmail.com>
2021-12-29 09:32:09 -0600

    move log message
    Signed-off-by: leecalcote <leecalcote@gmail.com>

--- main.go
+++ main.go
@@ -150,15 +150,15 @@ func serviceAddress() string {
 
 func registerCapabilities(port string, log logger.Handler) {
 	// Register workloads
-	log.Info("Registering static workloads...")
+	log.Info("Registering static workloads with Meshery Server...")
 	if err := oam.RegisterWorkloads(mesheryServerAddress(), serviceAddress()+":"+port); err != nil {
 		log.Info(err.Error())
 	}
-	log.Info("Registering static workloads completed")
 	// Register traits
 	if err := oam.RegisterTraits(mesheryServerAddress(), serviceAddress()+":"+port); err != nil {
 		log.Info(err.Error())
 	}
+	log.Info("Successfully registered static components with Meshery Server.")
 }
 
 func registerDynamicCapabilities(port string, log logger.Handler) {
