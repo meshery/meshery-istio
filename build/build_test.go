7db178ceb667796886f32e6d75d398afa034fb09
ashish <ashishjaitiwari15112000@gmail.com>
2022-01-18 01:39:30 +0530

    Removed redundant things
    Signed-off-by: ashish <ashishjaitiwari15112000@gmail.com>

--- build/build_test.go
+++ build/build_test.go
@@ -1,6 +1,8 @@
 package build
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/layer5io/meshery-adapter-library/adapter"
@@ -9,10 +11,11 @@ import (
 //TestCreateComponents can be used to directly invoke CreateComponents for writing new definitions and schemas onto the file system.
 // This test is left here only for development purposes
 func TestCreateComponents(t *testing.T) {
+	wd, _ := os.Getwd()
 	err := adapter.CreateComponents(adapter.StaticCompConfig{
 		URL:     DefaultGenerationURL,
 		Method:  DefaultGenerationMethod,
-		Path:    WorkloadPath,
+		Path:    filepath.Join(wd, "../templates", "oam", "workloads"),
 		DirName: LatestVersion,
 		Config:  NewConfig(LatestVersion),
 		Force:   true,
