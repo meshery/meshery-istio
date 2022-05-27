58ac7af5f1788c9b2194e55cedcbb85951c631ec
Nithish <nithishkarthik@Nithishs-MacBook-Pro.local>
2022-05-27 17:11:39 +0530

    refactor: Use cuelang for component generation
    Signed-off-by: Nithish <nithishkarthik@Nithishs-MacBook-Pro.local>

--- build/config.go
+++ build/config.go
@@ -1,11 +1,14 @@
 package build
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/layer5io/meshery-adapter-library/adapter"
 
+	"cuelang.org/go/cue"
 	"github.com/layer5io/meshkit/utils"
 	"github.com/layer5io/meshkit/utils/manifests"
 	smp "github.com/layer5io/service-mesh-performance/spec"
@@ -25,16 +28,50 @@ func NewConfig(version string) manifests.Config {
 		Name:        smp.ServiceMesh_Type_name[int32(smp.ServiceMesh_ISTIO)],
 		Type:        Component,
 		MeshVersion: version,
-		Filter: manifests.CrdFilter{
-			RootFilter:    []string{"$[?(@.kind==\"CustomResourceDefinition\")]"},
-			NameFilter:    []string{"$..[\"spec\"][\"names\"][\"kind\"]"},
-			VersionFilter: []string{"$[0]..spec.versions[0]"},
-			GroupFilter:   []string{"$[0]..spec"},
-			SpecFilter:    []string{"$[0]..openAPIV3Schema.properties.spec"},
-			ItrFilter:     []string{"$[?(@.spec.names.kind"},
-			ItrSpecFilter: []string{"$[?(@.spec.names.kind"},
-			VField:        "name",
-			GField:        "group",
+		CrdFilter: manifests.CueCrdFilter{
+			IdentifierExtractor: func(rootCRDCueVal cue.Value) (cue.Value, error) {
+				res := rootCRDCueVal.LookupPath(cue.ParsePath("spec.names.kind"))
+				if !res.Exists() {
+					return res, fmt.Errorf("Could not find the value")
+				}
+				return res.Value(), nil
+			},
+			NameExtractor: func(rootCRDCueVal cue.Value) (cue.Value, error) {
+				res := rootCRDCueVal.LookupPath(cue.ParsePath("spec.names.kind"))
+				if !res.Exists() {
+					return res, fmt.Errorf("Could not find the value")
+				}
+				return res.Value(), nil
+			},
+			VersionExtractor: func(rootCRDCueVal cue.Value) (cue.Value, error) {
+				res := rootCRDCueVal.LookupPath(cue.ParsePath("spec.versions[0].name"))
+				if !res.Exists() {
+					return res, fmt.Errorf("Could not find the value")
+				}
+				return res.Value(), nil
+			},
+			GroupExtractor: func(rootCRDCueVal cue.Value) (cue.Value, error) {
+				res := rootCRDCueVal.LookupPath(cue.ParsePath("spec.group"))
+				if !res.Exists() {
+					return res, fmt.Errorf("Could not find the value")
+				}
+				return res.Value(), nil
+			},
+			SpecExtractor: func(rootCRDCueVal cue.Value) (cue.Value, error) {
+				res := rootCRDCueVal.LookupPath(cue.ParsePath("spec.versions[0].schema.openAPIV3Schema.properties.spec"))
+				if !res.Exists() {
+					return res, fmt.Errorf("Could not find the value")
+				}
+				return res.Value(), nil
+			},
+		},
+		ExtractCrds: func(manifest string) []string {
+			crds := strings.Split(manifest, "---")
+			// trim the spaces
+			for _, crd := range crds {
+				crd = strings.TrimSpace(crd)
+			}
+			return crds
 		},
 	}
 }
